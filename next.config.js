dfb5d3338283974b0f2150ac9b40959ce0ff10db
Supreet Aradhya <fcb4days@gmail.com>
2025-08-20 01:48:56 -0500

    fixing deployment issues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
+/** @type {import('next').NextConfig} */
+
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  output: 'export',
+const nextConfig = {
+  output: 'export', // required for GitHub Pages
   basePath: isProd ? '/2025_portfolio' : '',
   assetPrefix: isProd ? '/2025_portfolio/' : '',
-  images: {
-    unoptimized: true,
+  images: { unoptimized: true },
+  experimental: {
+    appDir: true,
+  },
+  staticPageGenerationTimeout: 300,
+  trailingSlash: true,
+
+  async exportPathMap(defaultPathMap) {
+    // Remove /og from being exported
+    delete defaultPathMap['/og'];
+    return defaultPathMap;
   },
 };
+
+module.exports = nextConfig;
