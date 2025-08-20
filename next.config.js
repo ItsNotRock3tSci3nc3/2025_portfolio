01158b64e764282123b866e6fda671584ca3eccb
Supreet <33377709+ItsNotRock3tSci3nc3@users.noreply.github.com>
2025-08-20 01:54:45 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: 'export', // required for GitHub Pages
-  basePath: isProd ? '/2025_portfolio' : '',
-  assetPrefix: isProd ? '/2025_portfolio/' : '',
+  output: "export", // required for GitHub Pages
+  basePath: isProd ? "/2025_portfolio" : "",
+  assetPrefix: isProd ? "/2025_portfolio/" : "",
   images: { unoptimized: true },
-  experimental: {
-    appDir: true,
-  },
-  staticPageGenerationTimeout: 300,
   trailingSlash: true,
-
-  async exportPathMap(defaultPathMap) {
-    // Remove /og from being exported
-    delete defaultPathMap['/og'];
-    return defaultPathMap;
-  },
 };
 
 module.exports = nextConfig;
