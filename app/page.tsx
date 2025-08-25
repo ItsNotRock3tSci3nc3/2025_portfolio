917068a9798c377e7e8e1056a826579ac85f6421
Supreet Aradhya <fcb4days@gmail.com>
2025-08-25 01:13:55 -0500

    File path fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,8 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Hi! I'm Supreet Aradhya.
       </h1>
-      <Image src="2025_portfolio/Supreet_Sitting-2.JPEG" alt="Supreet Headshot" width={300} height={100} unoptimized/>
+      <img src="/2025_portfolio/Supreet_Sitting-2.JPEG" alt="Headshot" width={300} />
+
       <br />
       <p className="mb-4">
         I am a student at the University of Missouri – Columbia, pursuing a Bachelor’s degree in Information Technology with a minor in Computational Neuroscience.
@@ -16,7 +17,8 @@ export default function Page() {
           The process of building new ideas and overcoming challenges continually motivates me to take on new projects. My interests are diverse.
           I am always open to a challenge and trying something new.
           <br />
-          Feel free to explore my <a href="https://github.com/ItsNotRock3tSci3nc3" target="_blank" rel="noopener noreferrer"><span className="underline">GitHub</span></a> and view my <a href="/resume.pdf" target="_blank" rel="noopener noreferrer"><span className="underline">resume</span></a> to learn more about my work.
+          Feel free to explore my <a href="https://github.com/ItsNotRock3tSci3nc3" target="_blank" rel="noopener noreferrer"><span className="underline">GitHub</span></a> and view my <a href="/2025_portfolio/resume.pdf" target="_blank" rel="noopener noreferrer">Resume
+                                                                                                                                                                                      </a><span className="underline">resume</span></a> to learn more about my work.
           <br />
           Want to get in touch? You can reach me at <a href="mailto:aradhyasupreet@gmail.com"><span className="underline">aradhyasupreet@gmail.com</span></a>.
         </p>
