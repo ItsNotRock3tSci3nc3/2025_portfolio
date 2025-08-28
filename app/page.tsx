818bf4ba77c009cdd96cf828c57ad0065ca388d9
Supreet Aradhya <fcb4days@gmail.com>
2025-08-28 14:30:42 -0500

    Attachment fix for github pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,45 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Hi! I'm Supreet Aradhya.
       </h1>
-      <img src="/2025_portfolio/Supreet_Sitting-2.JPEG" alt="Headshot" width={300} />
-
+      {/*<img src="/2025_portfolio/Supreet_Sitting-2.JPEG" alt="Headshot" width={300} />*/}
+      <img src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/Supreet_Sitting-2.JPEG`} alt="Headshot" width={300}/>
       <br />
       <p className="mb-4">
         I am a student at the University of Missouri – Columbia, pursuing a Bachelor’s degree in Information Technology with a minor in Computational Neuroscience.
-          I have a strong passion for technology and enjoy working on personal projects inspired by what I learn in class, at work, or through independent study.
-          Since I was a kid, I've been a bit of a tinker. I'd always take on projects to create something new, or fix something broken.
-          The process of building new ideas and overcoming challenges continually motivates me to take on new projects. My interests are diverse.
-          I am always open to a challenge and trying something new.
-          <br />
-          Feel free to explore my <a href="https://github.com/ItsNotRock3tSci3nc3" target="_blank" rel="noopener noreferrer"><span className="underline">GitHub</span></a> and view my <a href="/2025_portfolio/resume.pdf" target="_blank" rel="noopener noreferrer">Resume
-                                                                                                                                                                                      </a><span className="underline">resume</span></a> to learn more about my work.
-          <br />
-          Want to get in touch? You can reach me at <a href="mailto:aradhyasupreet@gmail.com"><span className="underline">aradhyasupreet@gmail.com</span></a>.
-        </p>
-      <div className="my-8">
+        I have a strong passion for technology and enjoy working on personal projects inspired by what I learn in class, at work, or through independent study.
+        Since I was a kid, I've been a bit of a tinker. I'd always take on projects to create something new, or fix something broken.
+        The process of building new ideas and overcoming challenges continually motivates me to take on new projects. My interests are diverse.
+        I am always open to a challenge and trying something new.
+        <br />
+        Feel free to explore my{" "}
+        <a
+          href="https://github.com/ItsNotRock3tSci3nc3"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          GitHub
+        </a>{" "}
+        and view my{" "}
+        <a
+          href="/2025_portfolio/resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          Resume
+        </a>{" "}
+        to learn more about my work.
+        <br />
+        Want to get in touch? You can reach me at{" "}
+        <a href="mailto:aradhyasupreet@gmail.com" className="underline">
+          aradhyasupreet@gmail.com
+        </a>.
+      </p>
+
+      {/*<div className="my-8">*/}
         {/*<BlogPosts />*/}
-      </div>
+      {/*</div>*/}
     </section>
   )
 }
